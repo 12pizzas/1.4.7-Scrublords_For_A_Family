340db456faaaa3a50b0aebe9d1247ddc486fea10
12pizzas <monster.domo975@gmail.com>
2017-01-05 12:59:14 -0700

    Added code into my section of the file

---
+++ 1.4.7_Canopy_File.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+from __future__ import matplotlib.pyplot as plt
+from __future__ import os.path
+from __future__ import numpy as np
+
+if directory == None:
+    directory = os.cwd()
