7b3e6a9ff9acbf9ebd9711b0abb78431ba06e522
goldenpie <wrights@mynuames.org>
2017-01-11 12:52:56 -0700

    trying to make a way to choose imiges

--- goldenpie_file_1.4.7.py
+++ goldenpie_file_1.4.7.py
@@ -10,10 +10,9 @@ import numpy as np
 def alpha_down():
     directory = os.path.dirname(os.path.abspath(__file__)) 
     # Build an absolute filename from directory + filename
-    filename = os.path.join(directory,'mrclean.png')
+    filename = os.path.join(directory,'cow2.png')
     # Read the image data into an array
-    img = plt.imread(filename)
-    
+
     '''Show the image data'''
     # Create figure with 1 subplot
     fig, ax = plt.subplots(1, 1)
