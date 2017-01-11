f64a41f163f38be9563f8a1bf955c7633140ce67
goldenpie <wrights@mynuames.org>
2017-01-11 12:18:10 -0700

    prepared alpha system can change but strange collors appear

--- goldenpie_file_1.4.7.py
+++ goldenpie_file_1.4.7.py
@@ -12,7 +12,7 @@ import numpy as np
 def alpha_down():
     directory = os.path.dirname(os.path.abspath(__file__)) 
     # Build an absolute filename from directory + filename
-    filename = os.path.join(directory, 'cow.jpg')
+    filename = os.path.join(directory,'cow2.png')
     # Read the image data into an array
     img = plt.imread(filename)
     
@@ -28,5 +28,5 @@ def alpha_down():
     width = len(img[0])
     for r in range(height):
         for c in range(width):
-            img[r][c]=[0,0,0]
+            img[r][c]+=[0,0,0,127]
     ax.imshow(img, interpolation='none')
