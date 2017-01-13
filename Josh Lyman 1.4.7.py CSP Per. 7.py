04876df2be3d2e4d74a0194db358bdd13fed162f
joshlyman66 <joshlyman66@gmail.com>
2017-01-13 12:21:41 -0700

    Finished up the paste code

--- Josh Lyman 1.4.7.py CSP Per. 7.py
+++ Josh Lyman 1.4.7.py CSP Per. 7.py
@@ -11,14 +11,22 @@ def paste_img2():
     
     Take the second image that will be displayed on the first image
     and lowers the alpha channel or opaqueness.
-    Will paste the image onto the First Image in a certain position.'''
+    Will paste the image onto the First Image at the top right.'''
     
     #Open the files in the same directory as the Python script
     directory = os.path.dirname(os.path.abspath(__file__))
+    
+    #Define Images
+    img1_file = os.path.join(directory, 'cow2.png')
+    img1_img = PIL.Image.open(img1_file)
     img2_file = os.path.join(directory, 'mrclean.png')
-
-    #Show the new image in a new window
     img2_img = PIL.Image.open(img2_file)
+    img2_small = img2_img.resize((200,200))
+    img1_small = img1_img.resize((200,200))
     fig, ax = plt.subplots(1, 1)
+    
+    #Paste image 2 on image 1
+    img2_img.paste(img1_small, (550,25), mask=img1_small)
     ax.imshow(img2_img, interpolation='none')
+    
     fig.show()
